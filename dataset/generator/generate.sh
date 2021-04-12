b9d8847448e2bb0ad72463ee5f909c544d98e698
mocatfrio <hafarafirdausi@gmail.com>
2021-04-12 21:25:52 +0700

    Add data generator and datasets

--- dataset/generator/generate.sh
+++ dataset/generator/generate.sh
@@ -5,69 +5,66 @@
 #   rm -rf ${MY_DIR}*
 # fi
 
-# for precomputation efficiency
-# python3 main.py -t ind -n 300000 -d 3 -l product
-# python3 main.py -t ind -n 300000 -d 3 -l customer
-# python3 main.py -t ant -n 300000 -d 3 -l product
-# python3 main.py -t ant -n 300000 -d 3 -l customer
+# PRECOMPUTATION EFFICIENCY
 
-# python3 main.py -t ind -n 100000 -d 2 -l product
-# python3 main.py -t ind -n 100000 -d 2 -l customer
-# python3 main.py -t ind -n 100000 -d 5 -l product
-# python3 main.py -t ind -n 100000 -d 5 -l customer
-# python3 main.py -t ind -n 100000 -d 7 -l product
-# python3 main.py -t ind -n 100000 -d 7 -l customer
-# python3 main.py -t ant -n 100000 -d 2 -l product
-# python3 main.py -t ant -n 100000 -d 2 -l customer
-# python3 main.py -t ant -n 100000 -d 5 -l product
-# python3 main.py -t ant -n 100000 -d 5 -l customer
-# python3 main.py -t ant -n 100000 -d 7 -l product
-# python3 main.py -t ant -n 100000 -d 7 -l customer
+# GENERATE DATA
+# IND 
+python3 main.py -c generate -t ind -n 300000 -d 3 -l product
+python3 main.py -c generate -t ind -n 300000 -d 3 -l customer
+python3 main.py -c generate -t ind -n 100000 -d 2 -l product
+python3 main.py -c generate -t ind -n 100000 -d 2 -l customer
+python3 main.py -c generate -t ind -n 100000 -d 5 -l product
+python3 main.py -c generate -t ind -n 100000 -d 5 -l customer
+python3 main.py -c generate -t ind -n 100000 -d 7 -l product
+python3 main.py -c generate -t ind -n 100000 -d 7 -l customer
+# ANT 
+python3 main.py -c generate -t ant -n 300000 -d 3 -l product
+python3 main.py -c generate -t ant -n 300000 -d 3 -l customer
+python3 main.py -c generate -t ant -n 100000 -d 2 -l product
+python3 main.py -c generate -t ant -n 100000 -d 2 -l customer
+python3 main.py -c generate -t ant -n 100000 -d 5 -l product
+python3 main.py -c generate -t ant -n 100000 -d 5 -l customer
+python3 main.py -c generate -t ant -n 100000 -d 7 -l product
+python3 main.py -c generate -t ant -n 100000 -d 7 -l customer
 
-# # TEST KECIL2 
-python3 main.py -t ind -n 5000 -d 2 -l product
-python3 main.py -t ind -n 5000 -d 2 -l customer
-# python3 main.py -t ind -n 10000 -d 2 -l product
-# python3 main.py -t ind -n 10000 -d 2 -l customer
-# python3 main.py -t ind -n 5000 -d 3 -l product
-# python3 main.py -t ind -n 5000 -d 3 -l customer
-# python3 main.py -t ind -n 10000 -d 3 -l product
-# python3 main.py -t ind -n 10000 -d 3 -l customer
+# PREPARE DATA 
+# IND 
+python3 main.py -c prepare -f ind_300000_3_product.csv
+python3 main.py -c prepare -f ind_300000_3_customer.csv
+python3 main.py -c prepare -f ind_100000_2_product.csv
+python3 main.py -c prepare -f ind_100000_2_customer.csv
+python3 main.py -c prepare -f ind_100000_5_product.csv
+python3 main.py -c prepare -f ind_100000_5_customer.csv
+python3 main.py -c prepare -f ind_100000_7_product.csv
+python3 main.py -c prepare -f ind_100000_7_customer.csv
+# ANT 
+python3 main.py -c prepare -f ant_300000_3_product.csv
+python3 main.py -c prepare -f ant_300000_3_customer.csv
+python3 main.py -c prepare -f ant_100000_2_product.csv
+python3 main.py -c prepare -f ant_100000_2_customer.csv
+python3 main.py -c prepare -f ant_100000_5_product.csv
+python3 main.py -c prepare -f ant_100000_5_customer.csv
+python3 main.py -c prepare -f ant_100000_7_product.csv
+python3 main.py -c prepare -f ant_100000_7_customer.csv
 
-# # IND 
-# python3 main.py -t ind -n 50000 -d 3 -l product
-# python3 main.py -t ind -n 50000 -d 3 -l customer
-# python3 main.py -t ind -n 75000 -d 3 -l product
-# python3 main.py -t ind -n 75000 -d 3 -l customer
-# python3 main.py -t ind -n 100000 -d 3 -l product
-# python3 main.py -t ind -n 100000 -d 3 -l customer
-# python3 main.py -t ind -n 200000 -d 3 -l product
-# python3 main.py -t ind -n 200000 -d 3 -l customer
-# python3 main.py -t ind -n 300000 -d 3 -l product
-# python3 main.py -t ind -n 300000 -d 3 -l customer
-# python3 main.py -t ind -n 33000 -d 3 -l customer
-# python3 main.py -t ind -n 100000 -d 2 -l product
-# python3 main.py -t ind -n 100000 -d 2 -l customer
-# python3 main.py -t ind -n 100000 -d 5 -l product
-# python3 main.py -t ind -n 100000 -d 5 -l customer
-# python3 main.py -t ind -n 100000 -d 7 -l product
-# python3 main.py -t ind -n 100000 -d 7 -l customer
-
-# # ANT 
-# python3 main.py -t ant -n 50000 -d 3 -l product
-# python3 main.py -t ant -n 50000 -d 3 -l customer
-# python3 main.py -t ant -n 75000 -d 3 -l product
-# python3 main.py -t ant -n 75000 -d 3 -l customer
-# python3 main.py -t ant -n 100000 -d 3 -l product
-# python3 main.py -t ant -n 100000 -d 3 -l customer
-# python3 main.py -t ant -n 200000 -d 3 -l product
-# python3 main.py -t ant -n 200000 -d 3 -l customer
-# python3 main.py -t ant -n 300000 -d 3 -l product
-# python3 main.py -t ant -n 300000 -d 3 -l customer
-# python3 main.py -t ant -n 33000 -d 3 -l customer
-# python3 main.py -t ant -n 100000 -d 2 -l product
-# python3 main.py -t ant -n 100000 -d 2 -l customer
-# python3 main.py -t ant -n 100000 -d 5 -l product
-# python3 main.py -t ant -n 100000 -d 5 -l customer
-# python3 main.py -t ant -n 100000 -d 7 -l product
-# python3 main.py -t ant -n 100000 -d 7 -l customer
+# PARTITION DATA
+# IND 
+python3 main.py -c partition -f ind_300000_3_product.csv -n 200000
+python3 main.py -c partition -f ind_300000_3_customer.csv -n 200000
+python3 main.py -c partition -f ind_300000_3_product.csv -n 100000
+python3 main.py -c partition -f ind_300000_3_customer.csv -n 100000
+python3 main.py -c partition -f ind_300000_3_product.csv -n 75000
+python3 main.py -c partition -f ind_300000_3_customer.csv -n 75000
+python3 main.py -c partition -f ind_300000_3_product.csv -n 50000
+python3 main.py -c partition -f ind_300000_3_customer.csv -n 50000
+python3 main.py -c partition -f ind_300000_3_customer.csv -n 33000
+# ANT 
+python3 main.py -c partition -f ant_300000_3_product.csv -n 200000
+python3 main.py -c partition -f ant_300000_3_customer.csv -n 200000
+python3 main.py -c partition -f ant_300000_3_product.csv -n 100000
+python3 main.py -c partition -f ant_300000_3_customer.csv -n 100000
+python3 main.py -c partition -f ant_300000_3_product.csv -n 75000
+python3 main.py -c partition -f ant_300000_3_customer.csv -n 75000
+python3 main.py -c partition -f ant_300000_3_product.csv -n 50000
+python3 main.py -c partition -f ant_300000_3_customer.csv -n 50000
+python3 main.py -c partition -f ant_300000_3_customer.csv -n 33000
--- dataset/generator/main.py
+++ dataset/generator/main.py
@@ -4,6 +4,8 @@ import csv
 import math 
 import getopt
 import random
+
+import numpy as np
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -13,18 +15,19 @@ DISTANCE = 5
 
 
 def main(argv):
-    short_command = "ht:n:d:l:"
-    long_command = ["help", "dataset_type=", "data_size=", "dim_size=", "label="]
+    short_command = "hc:t:n:d:l:f:"
+    long_command = ["help", "command=", "dataset_type=", "data_size=", "dim_size=", "label=", "file="]
     try:
         opts, args = getopt.getopt(argv, short_command, long_command)
     except getopt.GetoptError:
-        print("main.py -t <dataset_type> -n <data_size> -d <dim_size> -l <label_data>")
+        print("main.py -c <command> -t <dataset_type> -n <data_size> -d <dim_size> -l <label_data> -f <filename>")
         sys.exit(2)
-    print(opts)
     for opt, arg in opts:
         if opt in ("-h", "--help"):
-            print("main.py -t <dataset_type> -n <data_size> -d <dim_size> -l <label_data>")
+            print("main.py -c <command> -t <dataset_type> -n <data_size> -d <dim_size> -l <label_data> -f <filename>")
             sys.exit()
+        elif opt in ("-c", "--command"):
+            command = arg
         elif opt in ("-t", "--dataset_type"):
             dataset_type = arg
         elif opt in ("-n", "--data_size"):
@@ -33,20 +36,72 @@ def main(argv):
             dim_size = int(arg)
         elif opt in ("-l", "--label"):
             label = arg
+        elif opt in ("-f", "--file"):
+            filename = os.getenv("DATASET_PATH") + arg
+    if command == "generate":
+        data = generate_data(dim_size, data_size, dataset_type, label)
+    elif command == "prepare":
+        data = prepare_data(filename)
+    elif command == "partition":
+        data = partition_data(filename, data_size)
+        dataset_type = filename.split("_")[0]
+        dim_size = filename.split("_")[2]
+        label = filename.split("_")[3]
+        filename = "_".join([dataset_type, str(data_size), dim_size, label])
+    if not "filename" in locals():
+        # export data 
+        filename = "_".join([dataset_type, str(data_size), str(dim_size), label])
+        filename = os.getenv("DATASET_PATH") + filename + ".csv"
+    export_data(filename, data)
+
+def generate_data(dim_size, data_size, dataset_type, label):
     # generate data 
-    data = [["id", "label", "ts_in", "ts_out"]]
-    data[0] += ["attr_" + str(i+1) for i in range(dim_size)]
+    columns_name = ["id", "label", "ts_in", "ts_out"] + ["attr_" + str(i+1) for i in range(dim_size)]
+    data = [columns_name]
     for i in range(data_size):
         row = [i + 1, label + "-" + str(i + 1)]
         row += randomize_ts()
         row += randomize_data(dataset_type, dim_size)
         data.append(row)
-    filename = "_".join([dataset_type, str(data_size), str(dim_size), label])
-    csvfile = os.getenv("DATASET_PATH") + filename + ".csv"
-    with open(csvfile, "w") as output:
-        writer = csv.writer(output, lineterminator='\n')
-        writer.writerows(data)
-    print(csvfile)
+    return data
+
+def prepare_data(filename):
+    data = []
+    result = []
+    data_temp = {}
+    with open(filename, "r") as csv_file:
+        first_row = True
+        for row in csv_file:
+            if first_row: 
+                result.append([val.replace('\n', '') for val in row.split(',')])
+                first_row = False
+                continue
+            col = row.split(',')[1:]
+            # set name 
+            data_id = len(data)
+            data_temp[data_id] = {
+                "label": col[0],
+                "value": [val.replace('\n', '') for val in col[3:]]
+            }
+            data.append([int(val) for val in col[1:3]] + [data_id])
+    # sorting ts in
+    data = sort_data(data)
+    # recombine 
+    for i in range(len(data)):
+        temp = [i + 1, data_temp[data[i][-1]]["label"]] + [data[i][0], data[i][1]] + data_temp[data[i][-1]]["value"]
+        result.append(temp)
+    return result
+
+def partition_data(filename, data_size):
+    result = []
+    with open(filename, "r") as csv_file:
+        i = 0
+        for row in csv_file:
+            if i > data_size:
+                break
+            result.append([val.replace('\n', '') for val in row.split(',')])
+            i += 1
+    return result
 
 def randomize(arg=None):
     range_start = random.randint(0, MAX_VALUE) 
@@ -98,6 +153,21 @@ def is_independent(dataset_type):
 
 def is_anticorrelated(dataset_type):
     return dataset_type.lower() == "ant"
+
+def export_data(filename, data):
+    try:
+        with open(filename, "w") as output:
+            writer = csv.writer(output, lineterminator='\n')
+            writer.writerows(data)
+        print("Exporting data", filename, "is success!")
+    except Exception as e:
+        print("Error due to", e)
+
+def sort_data(data):
+    arr = list(map(tuple, data)) 
+    dt = np.dtype([("ts_in", np.int32), ("ts_out", np.int32), ("data_id", np.int32)])
+    arr = np.array(arr, dtype=dt)
+    return list(map(list, np.sort(arr, order=["ts_in"]).tolist()))
 
            
 if __name__ == '__main__':

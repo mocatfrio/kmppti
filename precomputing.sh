fea5cd74bea659de837cbc24c8c04317086c650f
mocatfrio <hafarafirdausi@gmail.com>
2021-04-13 07:06:16 +0000

    Update gitignore

--- precomputing.sh
+++ precomputing.sh
@@ -3,51 +3,51 @@
 # yang paling lama 
 # compute first 
 python3 main.py -d precomputing -p ind_300000_3_product.csv -c ind_300000_3_customer.csv
-python3 main.py -d precomputing -p ant_300000_3_product.csv -c ant_300000_3_customer.csv
-python3 main.py -d precomputing -p fc_300000_3_product.csv -c fc_300000_3_customer.csv
+#python3 main.py -d precomputing -p ant_300000_3_product.csv -c ant_300000_3_customer.csv
+#python3 main.py -d precomputing -p fc_300000_3_product.csv -c fc_300000_3_customer.csv
 
-python3 main.py -d precomputing -p ind_100000_2_product.csv -c ind_100000_2_customer.csv
-python3 main.py -d precomputing -p ant_100000_2_product.csv -c ant_100000_2_customer.csv
-python3 main.py -d precomputing -p fc_100000_2_product.csv -c fc_100000_2_customer.csv
+#python3 main.py -d precomputing -p ind_100000_2_product.csv -c ind_100000_2_customer.csv
+#python3 main.py -d precomputing -p ant_100000_2_product.csv -c ant_100000_2_customer.csv
+#python3 main.py -d precomputing -p fc_100000_2_product.csv -c fc_100000_2_customer.csv
 
-python3 main.py -d precomputing -p ind_100000_7_product.csv -c ind_100000_7_customer.csv
-python3 main.py -d precomputing -p ant_100000_7_product.csv -c ant_100000_7_customer.csv
-python3 main.py -d precomputing -p fc_100000_7_product.csv -c fc_100000_7_customer.csv
+#python3 main.py -d precomputing -p ind_100000_7_product.csv -c ind_100000_7_customer.csv
+#python3 main.py -d precomputing -p ant_100000_7_product.csv -c ant_100000_7_customer.csv
+#python3 main.py -d precomputing -p fc_100000_7_product.csv -c fc_100000_7_customer.csv
 
-python3 main.py -d precomputing -p ind_100000_5_product.csv -c ind_100000_5_customer.csv
-python3 main.py -d precomputing -p ant_100000_5_product.csv -c ant_100000_5_customer.csv
-python3 main.py -d precomputing -p fc_100000_5_product.csv -c fc_100000_5_customer.csv
+#python3 main.py -d precomputing -p ind_100000_5_product.csv -c ind_100000_5_customer.csv
+#python3 main.py -d precomputing -p ant_100000_5_product.csv -c ant_100000_5_customer.csv
+#python3 main.py -d precomputing -p fc_100000_5_product.csv -c fc_100000_5_customer.csv
 
 # keknya ini bisa pake history 
-python3 main.py -d precomputing -p ind_100000_3_product.csv -c ind_33000_3_customer.csv
-python3 main.py -d precomputing -p ant_100000_3_product.csv -c ant_33000_3_customer.csv
-python3 main.py -d precomputing -p fc_100000_3_product.csv -c fc_33000_3_customer.csv
+#python3 main.py -d precomputing -p ind_100000_3_product.csv -c ind_33000_3_customer.csv
+#python3 main.py -d precomputing -p ant_100000_3_product.csv -c ant_33000_3_customer.csv
+#python3 main.py -d precomputing -p fc_100000_3_product.csv -c fc_33000_3_customer.csv
 
-python3 main.py -d precomputing -p ind_100000_3_product.csv -c ind_300000_3_customer.csv
-python3 main.py -d precomputing -p ant_100000_3_product.csv -c ant_300000_3_customer.csv
-python3 main.py -d precomputing -p fc_100000_3_product.csv -c fc_300000_3_customer.csv
+#python3 main.py -d precomputing -p ind_100000_3_product.csv -c ind_300000_3_customer.csv
+#python3 main.py -d precomputing -p ant_100000_3_product.csv -c ant_300000_3_customer.csv
+#python3 main.py -d precomputing -p fc_100000_3_product.csv -c fc_300000_3_customer.csv
 
-python3 main.py -d precomputing -p ind_100000_3_product.csv -c ind_200000_3_customer.csv
-python3 main.py -d precomputing -p ant_100000_3_product.csv -c ant_200000_3_customer.csv
-python3 main.py -d precomputing -p fc_100000_3_product.csv -c fc_200000_3_customer.csv
+#python3 main.py -d precomputing -p ind_100000_3_product.csv -c ind_200000_3_customer.csv
+#python3 main.py -d precomputing -p ant_100000_3_product.csv -c ant_200000_3_customer.csv
+#python3 main.py -d precomputing -p fc_100000_3_product.csv -c fc_200000_3_customer.csv
 
-python3 main.py -d precomputing -p ind_100000_3_product.csv -c ind_50000_3_customer.csv
-python3 main.py -d precomputing -p ant_100000_3_product.csv -c ant_50000_3_customer.csv
-python3 main.py -d precomputing -p fc_100000_3_product.csv -c fc_50000_3_customer.csv
+#python3 main.py -d precomputing -p ind_100000_3_product.csv -c ind_50000_3_customer.csv
+#python3 main.py -d precomputing -p ant_100000_3_product.csv -c ant_50000_3_customer.csv
+#python3 main.py -d precomputing -p fc_100000_3_product.csv -c fc_50000_3_customer.csv
 
 # pake history 
-python3 main.py -d precomputing -p ind_200000_3_product.csv -c ind_200000_3_customer.csv
-python3 main.py -d precomputing -p ant_200000_3_product.csv -c ant_200000_3_customer.csv
-python3 main.py -d precomputing -p fc_200000_3_product.csv -c fc_200000_3_customer.csv
+#python3 main.py -d precomputing -p ind_200000_3_product.csv -c ind_200000_3_customer.csv
+#python3 main.py -d precomputing -p ant_200000_3_product.csv -c ant_200000_3_customer.csv
+#python3 main.py -d precomputing -p fc_200000_3_product.csv -c fc_200000_3_customer.csv
 # pake history 
-python3 main.py -d precomputing -p ind_100000_3_product.csv -c ind_100000_3_customer.csv
-python3 main.py -d precomputing -p ant_100000_3_product.csv -c ant_100000_3_customer.csv
-python3 main.py -d precomputing -p fc_100000_3_product.csv -c fc_100000_3_customer.csv
+#python3 main.py -d precomputing -p ind_100000_3_product.csv -c ind_100000_3_customer.csv
+#python3 main.py -d precomputing -p ant_100000_3_product.csv -c ant_100000_3_customer.csv
+#python3 main.py -d precomputing -p fc_100000_3_product.csv -c fc_100000_3_customer.csv
 # pake history 
-python3 main.py -d precomputing -p ind_75000_3_product.csv -c ind_75000_3_customer.csv
-python3 main.py -d precomputing -p ant_75000_3_product.csv -c ant_75000_3_customer.csv
-python3 main.py -d precomputing -p fc_75000_3_product.csv -c fc_75000_3_customer.csv
+#python3 main.py -d precomputing -p ind_75000_3_product.csv -c ind_75000_3_customer.csv
+#python3 main.py -d precomputing -p ant_75000_3_product.csv -c ant_75000_3_customer.csv
+#python3 main.py -d precomputing -p fc_75000_3_product.csv -c fc_75000_3_customer.csv
 # pake history 
-python3 main.py -d precomputing -p ind_50000_3_product.csv -c ind_50000_3_customer.csv
-python3 main.py -d precomputing -p ant_50000_3_product.csv -c ant_50000_3_customer.csv
-python3 main.py -d precomputing -p fc_50000_3_product.csv -c fc_50000_3_customer.csv
+#python3 main.py -d precomputing -p ind_50000_3_product.csv -c ind_50000_3_customer.csv
+#python3 main.py -d precomputing -p ant_50000_3_product.csv -c ant_50000_3_customer.csv
+#python3 main.py -d precomputing -p fc_50000_3_product.csv -c fc_50000_3_customer.csv

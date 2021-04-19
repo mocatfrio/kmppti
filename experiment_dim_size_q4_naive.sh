d0da27844e69ecbaf0982fa51bc78bd771057d98
mocatfrio <hafarafirdausi@gmail.com>
2021-04-19 23:30:41 +0700

    Add dataset and change scenario

--- experiment_dim_size_q4_naive.sh
+++ experiment_dim_size_q4_naive.sh
@@ -2,15 +2,15 @@
 
 # dimension size experiments
 # IND 
-python3 main.py -m naive -p ind_5000_2_product.csv -c ind_5000_2_customer.csv -k 50 -g 3 -s 100 -e 200
-python3 main.py -m naive -p ind_5000_5_product.csv -c ind_5000_5_customer.csv -k 50 -g 3 -s 100 -e 200
-python3 main.py -m naive -p ind_5000_7_product.csv -c ind_5000_7_customer.csv -k 50 -g 3 -s 100 -e 200
+python3 main.py -m naive -p ind_2000_2_product.csv -c ind_2000_2_customer.csv -k 50 -g 3 -s 100 -e 200
+python3 main.py -m naive -p ind_2000_5_product.csv -c ind_2000_5_customer.csv -k 50 -g 3 -s 100 -e 200
+python3 main.py -m naive -p ind_2000_7_product.csv -c ind_2000_7_customer.csv -k 50 -g 3 -s 100 -e 200
 # ANT 
-python3 main.py -m naive -p ant_5000_2_product.csv -c ant_5000_2_customer.csv -k 50 -g 3 -s 100 -e 200
-python3 main.py -m naive -p ant_5000_5_product.csv -c ant_5000_5_customer.csv -k 50 -g 3 -s 100 -e 200
-python3 main.py -m naive -p ant_5000_7_product.csv -c ant_5000_7_customer.csv -k 50 -g 3 -s 100 -e 200
+python3 main.py -m naive -p ant_2000_2_product.csv -c ant_2000_2_customer.csv -k 50 -g 3 -s 100 -e 200
+python3 main.py -m naive -p ant_2000_5_product.csv -c ant_2000_5_customer.csv -k 50 -g 3 -s 100 -e 200
+python3 main.py -m naive -p ant_2000_7_product.csv -c ant_2000_7_customer.csv -k 50 -g 3 -s 100 -e 200
 # FC
-python3 main.py -m naive -p fc_5000_2_product.csv -c fc_5000_2_customer.csv -k 50 -g 3 -s 100 -e 200
-python3 main.py -m naive -p fc_5000_5_product.csv -c fc_5000_5_customer.csv -k 50 -g 3 -s 100 -e 200
-python3 main.py -m naive -p fc_5000_7_product.csv -c fc_5000_7_customer.csv -k 50 -g 3 -s 100 -e 200
+python3 main.py -m naive -p fc_2000_2_product.csv -c fc_2000_2_customer.csv -k 50 -g 3 -s 100 -e 200
+python3 main.py -m naive -p fc_2000_5_product.csv -c fc_2000_5_customer.csv -k 50 -g 3 -s 100 -e 200
+python3 main.py -m naive -p fc_2000_7_product.csv -c fc_2000_7_customer.csv -k 50 -g 3 -s 100 -e 200
 
--- experiment_dim_size_q4_onkmppti.sh
+++ experiment_dim_size_q4_onkmppti.sh
@@ -2,15 +2,15 @@
 
 # dimension size experiments
 # IND 
-python3 main.py -m online_kmppti -p ind_5000_2_product.csv -c ind_5000_2_customer.csv -k 50 -g 3 -s 100 -e 200
-python3 main.py -m online_kmppti -p ind_5000_5_product.csv -c ind_5000_5_customer.csv -k 50 -g 3 -s 100 -e 200
-python3 main.py -m online_kmppti -p ind_5000_7_product.csv -c ind_5000_7_customer.csv -k 50 -g 3 -s 100 -e 200
+python3 main.py -m online_kmppti -p ind_2000_2_product.csv -c ind_2000_2_customer.csv -k 50 -g 3 -s 100 -e 200
+python3 main.py -m online_kmppti -p ind_2000_5_product.csv -c ind_2000_5_customer.csv -k 50 -g 3 -s 100 -e 200
+python3 main.py -m online_kmppti -p ind_2000_7_product.csv -c ind_2000_7_customer.csv -k 50 -g 3 -s 100 -e 200
 # ANT 
-python3 main.py -m online_kmppti -p ant_5000_2_product.csv -c ant_5000_2_customer.csv -k 50 -g 3 -s 100 -e 200
-python3 main.py -m online_kmppti -p ant_5000_5_product.csv -c ant_5000_5_customer.csv -k 50 -g 3 -s 100 -e 200
-python3 main.py -m online_kmppti -p ant_5000_7_product.csv -c ant_5000_7_customer.csv -k 50 -g 3 -s 100 -e 200
+python3 main.py -m online_kmppti -p ant_2000_2_product.csv -c ant_2000_2_customer.csv -k 50 -g 3 -s 100 -e 200
+python3 main.py -m online_kmppti -p ant_2000_5_product.csv -c ant_2000_5_customer.csv -k 50 -g 3 -s 100 -e 200
+python3 main.py -m online_kmppti -p ant_2000_7_product.csv -c ant_2000_7_customer.csv -k 50 -g 3 -s 100 -e 200
 # FC
-python3 main.py -m online_kmppti -p fc_5000_2_product.csv -c fc_5000_2_customer.csv -k 50 -g 3 -s 100 -e 200
-python3 main.py -m online_kmppti -p fc_5000_5_product.csv -c fc_5000_5_customer.csv -k 50 -g 3 -s 100 -e 200
-python3 main.py -m online_kmppti -p fc_5000_7_product.csv -c fc_5000_7_customer.csv -k 50 -g 3 -s 100 -e 200
+python3 main.py -m online_kmppti -p fc_2000_2_product.csv -c fc_2000_2_customer.csv -k 50 -g 3 -s 100 -e 200
+python3 main.py -m online_kmppti -p fc_2000_5_product.csv -c fc_2000_5_customer.csv -k 50 -g 3 -s 100 -e 200
+python3 main.py -m online_kmppti -p fc_2000_7_product.csv -c fc_2000_7_customer.csv -k 50 -g 3 -s 100 -e 200
 
